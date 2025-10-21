e11c165f3186142317a6c78cb0dc380975ca0655
Remi's MacBook <remi@Remis-MacBook-Air.local>
2025-10-21 11:16:12 -0500

    Updated to dark print + landscape version

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Content-Security-Policy':
+        "frame-ancestors https://carrierintelligence.com https://www.carrierintelligence.com https://*.gohighlevel.com https://*.myhighlevel.com",
+    },
+  },
 })
